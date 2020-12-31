c0945c23afe9f7e65f0bbade7e69e4595baa9047
Max <maxim.macari.dam@gmail.com>
2020-12-31 20:24:39 +0100

    Initial Commit

---
+++ InstagramFeedLayout/InstagramFeedLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramFeedLayoutApp.swift
+//  InstagramFeedLayout
+//
+//  Created by Maxim Macari on 31/12/20.
+//
+
+import SwiftUI
+
+@main
+struct InstagramFeedLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
